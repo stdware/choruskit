8ddb558761ff9b72454705f996244e198bea066e
CrSjimo <crs_16423@outlook.com>
2026-07-27 01:06:32 +0800

    Update translation

--- share/translations/choruskit_en_US.ts
+++ share/translations/choruskit_en_US.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../../src/loader/src/main.cpp" line="176"/>
         <source>Failed to load core: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Failed to load core: %1</translation>
     </message>
     <message>
         <location filename="../../src/loader/src/main.cpp" line="253"/>
         <source>You&apos;re trying to start %1 as the %2, which is extremely dangerous and therefore strongly not recommended.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">You&apos;re trying to start %1 as the %2, which is extremely dangerous and therefore strongly not recommended.</translation>
     </message>
     <message>
         <location filename="../../src/loader/src/main.cpp" line="258"/>
         <source>Administrator</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Administrator</translation>
     </message>
     <message>
         <location filename="../../src/loader/src/main.cpp" line="260"/>
         <source>Root</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Root</translation>
     </message>
     <message>
         <location filename="../../src/loader/src/main.cpp" line="299"/>
         <source>Searching plugins...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Searching plugins...</translation>
     </message>
     <message>
         <location filename="../../src/loader/src/main.cpp" line="333"/>
         <source>Could not find Core plugin!</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Could not find Core plugin!</translation>
     </message>
     <message>
         <location filename="../../src/loader/src/main.cpp" line="338"/>
         <source>Core plugin is disabled.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Core plugin is disabled.</translation>
     </message>
     <message>
         <location filename="../../src/loader/src/main.cpp" line="397"/>
         <source>Loading plugins...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Loading plugins...</translation>
     </message>
 </context>
 <context>
@@ -50,32 +50,32 @@
         <location filename="../../src/appcore/doc/documentsystem.cpp" line="319"/>
         <location filename="../../src/appcore/doc/documentsystem.cpp" line="426"/>
         <source>All Files</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">All Files</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentsystem.cpp" line="366"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Open File</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentsystem.cpp" line="380"/>
         <source>Open Files</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Open Files</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentsystem.cpp" line="393"/>
         <source>Open Directory</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Open Directory</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentsystem.cpp" line="408"/>
         <source>Save File</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Save File</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentsystem.cpp" line="447"/>
         <source>Save As File</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Save As File</translation>
     </message>
 </context>
 <context>
@@ -85,37 +85,37 @@
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="177"/>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="447"/>
         <source>File Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">File Error</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="158"/>
         <source>Cannot save %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Cannot save %1</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="160"/>
         <source>Error while saving %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Error while saving %1: %2</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="178"/>
         <source>%1 has been opened in the editor!</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">%1 has been opened in the editor!</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="449"/>
         <source>Cannot reload %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Cannot reload %1</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="451"/>
         <source>Error while reloading %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Error while reloading %1: %2</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="748"/>
         <source>Save As File</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Save As File</translation>
     </message>
 </context>
 <context>
@@ -123,57 +123,57 @@
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="749"/>
         <source>All Files</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">All Files</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="793"/>
         <source>File Changed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">File Changed</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="797"/>
         <source>The unsaved file %1 has changed outside. Do you want to reload it and discard your changes?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">The unsaved file %1 has changed outside. Do you want to reload it and discard your changes?</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="801"/>
         <source>The file %1 has changed outside. Do you want to reload it?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">The file %1 has changed outside. Do you want to reload it?</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="835"/>
         <source>File has been removed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">File has been removed</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="839"/>
         <source>The file %1 has been removed outside. Do you want to save it under a different name, or close the editor?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">The file %1 has been removed outside. Do you want to save it under a different name, or close the editor?</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="846"/>
         <source>The file %1 was removed. Do you want to save it under a different name, or close the editor?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">The file %1 was removed. Do you want to save it under a different name, or close the editor?</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="855"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;Close</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="857"/>
         <source>C&amp;lose All</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">C&amp;lose All</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="860"/>
         <source>Save &amp;as...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Save &amp;as...</translation>
     </message>
     <message>
         <location filename="../../src/appcore/doc/documentwatcher.cpp" line="863"/>
         <source>&amp;Save</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;Save</translation>
     </message>
 </context>
 </TS>
